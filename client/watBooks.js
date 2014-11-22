03988b1aad44afb7b557442f8abb9c697db34335
constancegervais <constance.gervais@gmail.com>
2014-11-21 22:17:35 -0500

    part 1 of amazon api

--- client/watBooks.js
+++ client/watBooks.js
@@ -13,6 +13,25 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       Session.set("counter", Session.get("counter") + 1);
+      console.log("hello");
+
+      Meteor.call('findBook', function(err, data) {
+        debugger;
+        if (err)
+          console.log(err);
+
+        console.log(data);
+        });
+
+      // var aws = Meteor.npmRequire("aws-lib");
+
+      // var prodAdv = aws.createProdAdvClient("AKIAJ2A6PMEPXER4AXXA", "NKpv7qB6Itl4c6O9/D4Z3o5ijQMbDkob0+TqxCcb", 'watdoyounee-20');
+
+      // var options = {SearchIndex: "Books", Keywords: "Javascript"}
+
+      // prodAdv.call("ItemSearch", options, function(err, result) {
+      //   console.log(result);
+      // })
     }
   });
 
@@ -21,5 +40,43 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+}
+
+// if (Meteor.isClient) {
+
+//   // results = function findBook(user, callback) {
+//   //   Meteor.call('findBook', user, callback);
+//   // }
+
+//   Meteor.call('findBook', function(err, data) {
+//   if (err)
+//     console.log(err);
+
+//   console.log(data);
+//   });
+
+// }
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    findBook: function() {
+      var AWS = Meteor.npmRequire("aws-lib");
+      var aws = new AWS({
+        version:"0.3.0"
+      });
+
+      var prodAdv = aws.createProdAdvClient("AKIAJ2A6PMEPXER4AXXA", "NKpv7qB6Itl4c6O9/D4Z3o5ijQMbDkob0+TqxCcb", 'watdoyounee-20');
+
+      var options = {SearchIndex: "Books", Keywords: "Javascript"}
+      var bookresults;
+
+      prodAdv.call("ItemSearch", options, function(err, result) {
+        bookresults = result;
+        console.log(result);
+      })
+
+      return result;
+    }
   });
 }
--- model.js
+++ model.js
@@ -1,41 +1,4 @@
 ////////// Shared code (client and server) //////////
 
 UWBooks = new Meteor.Collection('uwBooks');
-FedBooks = new Meteor.Collection('fedsBooks');
-
-
-Meteor.methods({
-  createClub: function (clubName, clubDescription, clubCategory, clubTag) {
-    var club = FedsClubs.findOne({clubName: clubName});
-
-    if (club) {
-      return club._id;
-    }
-    else {
-      return Clubs.insert({
-        createdAt: (new Date()),
-        clubName: clubName,
-        members: [this.userId],
-        clubDescription: clubDescription,
-        clubCategory: clubCategory,
-        clubTag: clubTag,
-        PersonalityTest: PersonalityTest
-      })
-    }
-  },
-
-  joinClub: function (clubName) {
-    var club = FedsClubs.findOne({clubName: clubName});
-
-    if (!club) {
-      return 'group doesn\'t exist';
-    }
-
-    if (_(club.members).contains(this.userId)) {
-      return 'already in group';
-    }
-
-    return FedsClubs.update(club, {$push: {members: this.userId}});
-  }
-
-});
+FedBooks = new Meteor.Collection('fedBooks');
